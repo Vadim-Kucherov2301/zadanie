262e788ca91a9d31455bea91b469838552ec01a7
Vadim Kucherov <vadim.kucherov23@gmail.com>
2024-04-15 08:19:05 +0300

    задание

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-function calculate(operation) {
-    const num1 = parseFloat(document.querySelectorAll(".input-field")[0].value);
-    const num2 = parseFloat(document.querySelectorAll(".input-field")[1].value);
-    let result;
-    if (operation === '+') {
-        result = num1 + num2;
-    } else if (operation === '-') {
-        result = num1 - num2;
-    }
-    document.getElementById("result").innerText = "Результат: " + result;
+
+
+let a = prompt("Введите число A");
+let b = prompt("Введите число B");
+
+a = parseFloat(a);
+b = parseFloat(b);
+
+function sum(a, b) {
+    return a + b;
 }
 
-function calculateResult() {
-    alert("Результат: " + document.getElementById("result").innerText.split(": ")[1]);
-}
+let res = sum(a, b);
+console.log(res);
